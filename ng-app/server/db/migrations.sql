e2bde22a5c9fb86c7c7f27166519f624d4a40f0e
lindseygilmore <lindseygilmore21@gmail.com>
2017-07-21 16:19:36 -0500

    server folders completed

---
+++ ng-app/server/db/migrations.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE <dbname>;
+
+\c <dbname>;
+
+CREATE TABLE <tblname>
+
+CREATE TABLE <tblname>
+
+#to add something to a table:
+INSERT INTO <tblname> (name, size, color, price, hooded, image_url)
+VALUES ();
+
+#to add a column to a table
+ALTER TABLE <tblname> ADD COLUMN token VARCHAR(255);
