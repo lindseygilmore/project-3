f8b634ef02d835e27b6ca6f5e11b20808751288a
lindseygilmore <lindseygilmore21@gmail.com>
2017-07-24 09:33:23 -0500

    config.ru requirements set

--- ng-app/server/db/migrations.sql
+++ ng-app/server/db/migrations.sql
@@ -1,14 +1,15 @@
-CREATE DATABASE <dbname>;
+CREATE DATABASE events;
 
-\c <dbname>;
+\c events;
 
-CREATE TABLE <tblname>
+CREATE TABLE event (id SERIAL PRIMARY KEY, name VARCHAR (255), company_hosting VARCHAR(255), price VARCHAR(255), location VARCHAR (255), event_date VARCHAR (255), city_id INT);
 
-CREATE TABLE <tblname>
+CREATE TABLE city (id SERIAL PRIMARY KEY, name VARCHAR(255), state VARCHAR(255));
 
+CREATE TABLE users (id SERIAL PRIMARY KEY, email VARCHAR(255), password_digest VARCHAR(255), token VARCHAR(255));
 #to add something to a table:
-INSERT INTO <tblname> (name, size, color, price, hooded, image_url)
-VALUES ();
+-- INSERT INTO <tblname> (name, size, color, price, hooded, image_url)
+-- VALUES ();
 
 #to add a column to a table
-ALTER TABLE <tblname> ADD COLUMN token VARCHAR(255);
+-- ALTER TABLE <tblname> ADD COLUMN token VARCHAR(255);
