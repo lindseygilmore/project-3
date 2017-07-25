41d6432bb796fe92a5ff290e171cf1590dc85f9b
lindseygilmore <lindseygilmore21@gmail.com>
2017-07-25 11:21:17 -0500

    image_url added and looping through client side

--- ng-app/server/db/migrations.sql
+++ ng-app/server/db/migrations.sql
@@ -7,3 +7,9 @@ CREATE TABLE event (id SERIAL PRIMARY KEY, name VARCHAR (255), company_hosting V
 CREATE TABLE city (id SERIAL PRIMARY KEY, name VARCHAR(255), state VARCHAR(255));
 
 CREATE TABLE users (id SERIAL PRIMARY KEY, email VARCHAR(255), password_digest VARCHAR(255), token VARCHAR(255));
+
+
+ALTER TABLE event ADD COLUMN image_url VARCHAR(255);
+
+INSERT INTO event (name, company_hosting, price, location, event_date, city_id, image_url) 
+VALUES ('Beach Boys', 'Wrigley', '20.00', 'Chicago, IL', '8/8/17', 3, 'http://img.wennermedia.com/920-width/rs-135002-square.jpg');
