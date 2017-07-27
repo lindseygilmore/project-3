f79417805477285748c553988bcc66b07b0bfecf
lindseygilmore <lindseygilmore21@gmail.com>
2017-07-27 15:12:13 -0500

    sign up function working

--- ng-app/server/controllers/ApplicationController.rb
+++ ng-app/server/controllers/ApplicationController.rb
@@ -10,16 +10,18 @@ class ApplicationController < Sinatra::Base
     response['Access-Control-Allow-Origin'] = '*'
     content_type :json
 
-    # path = request.fullpath.split("?")[0]
-    # if ['/users/login','/users/register'].include?(path) || request.request_method == 'OPTIONS'
-    #   pass
-    # end
-    # token = params[:token]
-    # user = User.find_by(token: token)
-    # if user
-    #   pass
-    # else
-    #   halt 403
-    # end
+    path = request.fullpath.split("?")[0]
+    if ['/users/login','/users/register'].include?(path) || request.request_method == 'OPTIONS'
+      pass
+    end
+
+    token = params[:token]
+    user = User.find_by(token: token)
+
+    if user
+      pass
+    else
+      p "you may not pass"
+    end
   end
 end
